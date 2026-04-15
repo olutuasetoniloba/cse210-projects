75c6025b7f80e89f3e08d0904f43a55dec9f559a
olutuasetoniloba <tolutuase@byupathway.edu>
2026-04-15 23:00:24 +0100

    week07

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,18 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Nov 2022", 30, 3.0));
+        activities.Add(new Cycling("03 Nov 2022", 30, 6.0));
+        activities.Add(new Swimming("03 Nov 2022", 30, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
+
+
